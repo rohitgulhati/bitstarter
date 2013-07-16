623e2846ca4f7ce5cbef15e5ef9bab7b4e817cba
Rohit Gulhati <rohitgulhati@gmail.com>
2013-07-16 20:50:03 +0000

    Updated to index.html 2

--- web.js
+++ web.js
@@ -3,8 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fs.readFileSync('./index.html','utf8', function(err,text) {
-     response.send(text)};
+  fs.readFileSync('./index.html','utf8', function(err, text) {response.send(text);
+  };
   });
 
 var port = process.env.PORT || 5000;
